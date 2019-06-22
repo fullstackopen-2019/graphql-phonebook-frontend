34c1d28a57be6257fbdae46308b0ee3d02a5ba3b
Matti Luukkainen <mluukkai@iki.fi>
2019-06-22 15:20:30 +0300

    part8-2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
 import React from 'react'
-import { Query, ApolloConsumer } from 'react-apollo'
+import { Query, ApolloConsumer, Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const ALL_PERSONS = gql`
-{
-  allPersons  {
-    name
-    phone
-    id
+  {
+    allPersons  {
+      name
+      phone
+      id
+    }
   }
-}
+`
+
+const CREATE_PERSON = gql`
+  mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
+    addPerson(
+      name: $name,
+      street: $street,
+      city: $city,
+      phone: $phone
+    ) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
 `
 
 const App = () => {
   return (
-    <ApolloConsumer>
-      {(client =>
-        <Query query={ALL_PERSONS}>
-          {(result) =>
-            <Persons result={result} client={client} />
-          }
-        </Query>
-      )}
-    </ApolloConsumer>
+    <div>
+      <ApolloConsumer>
+        {(client =>
+          <Query query={ALL_PERSONS} >
+            {(result) =>
+              <Persons result={result} client={client} />
+            }
+          </Query>
+        )}
+      </ApolloConsumer>
+      <h2>create new</h2>
+      <Mutation
+        mutation={CREATE_PERSON}
+        refetchQueries={[{ query: ALL_PERSONS }]}
+      >
+        {(addPerson) =>
+          <PersonForm
+            addUser={addPerson}
+          />
+        }
+      </Mutation>
+    </div>
   )
 }
 
