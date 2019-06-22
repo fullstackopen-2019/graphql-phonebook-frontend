e1162c5a807000ebf8bb0b234e0295b938291f44
Matti Luukkainen <mluukkai@iki.fi>
2019-06-22 20:25:19 +0300

    part8-6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { gql } from 'apollo-boost'
-import { useQuery, useMutation } from '@apollo/react-hooks'
+import { useQuery, useMutation, useApolloClient } from '@apollo/react-hooks'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 const ALL_PERSONS = gql`
   {
@@ -47,9 +48,17 @@ const EDIT_NUMBER = gql`
     }
   }
 `
+const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
+  }
+`
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
 
   const handleError = (error) => {
     setErrorMessage(error.graphQLErrors[0].message)
@@ -57,6 +66,8 @@ const App = () => {
       setErrorMessage(null)
     }, 10000)
   }
+
+  const client = useApolloClient()
 
   const persons = useQuery(ALL_PERSONS)
 
@@ -66,6 +77,34 @@ const App = () => {
   })
 
   const [editNumber] = useMutation(EDIT_NUMBER)
+
+  const [login] = useMutation(LOGIN, {
+    onError: handleError
+  })
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const errorNotification = () => errorMessage &&
+    <div style={{ color: 'red' }}>
+      {errorMessage}
+    </div>
+
+  if (!token) {
+    return (
+      <div>
+        {errorNotification()}
+        <h2>Login</h2>
+        <LoginForm
+          login={login}
+          setToken={(token) => setToken(token)}
+        />
+      </div>
+    )
+  }
 
   return (
     <div>
@@ -81,6 +120,8 @@ const App = () => {
 
       <h2>change number</h2>
       <PhoneForm editNumber={editNumber} />
+
+      <button onClick={logout}>logout</button>
     </div>
   )
 }
